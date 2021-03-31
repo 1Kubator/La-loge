80e89b6e4724bb83a8c972c4aaaca70e32c22cc9
Vidit <vidit.k@surya-soft.com>
2021-03-31 18:16:02 +0530

    Initlialize firebase

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:la_loge/resources/app_theme.dart';
 import 'package:la_loge/ui/login/login_screen.dart';
@@ -6,6 +7,8 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    WidgetsFlutterBinding.ensureInitialized();
+    Firebase.initializeApp();
     return MaterialApp(
       theme: AppTheme.get,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
