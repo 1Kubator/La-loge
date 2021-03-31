b3afd581110fc9db137106c5bc69777f729ce9ad
Vidit <vidit.k@surya-soft.com>
2021-03-31 18:23:57 +0530

    Create success and error dialogs

---
+++ lib/widgets/dialog_box.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:la_loge/error_handling/network_error.dart';
+
+class DialogBox {
+  static showSuccessDialog(context, msg) {
+    return showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Success'),
+        content: Text(msg),
+        actions: [
+          FlatButton(onPressed: () => Navigator.pop(context), child: Text('Ok'))
+        ],
+      ),
+    );
+  }
+
+  static showErrorDialog(context, e) {
+    final errorMsg = NetworkErrorMessage.getValue(context, e);
+    return showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Error'),
+        content: Text(errorMsg),
+        actions: [
+          FlatButton(onPressed: () => Navigator.pop(context), child: Text('Ok'))
+        ],
+      ),
+    );
+  }
+}
