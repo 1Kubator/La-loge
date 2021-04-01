027c816d8f4d584d1cbc11a516d88cd74693577b
Viditk <79438214+Viditk@users.noreply.github.com>
2021-04-01 14:09:27 +0530

    Add localisation files (#4)

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:la_loge/resources/app_theme.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: AppTheme.get,
     );
   }
