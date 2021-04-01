1fa725b51096ba43bc37ae9675864e2827ff8f0e
Viditk <79438214+Viditk@users.noreply.github.com>
2021-04-01 10:09:39 +0530

    Add theme (#2)

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:la_loge/resources/app_theme.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: AppTheme.get,
+    );
   }
 }
