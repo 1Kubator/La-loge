694d152cf6e098236b028482c3ba5de5f9c9523a
Viditkhanna <vidit.k@surya-soft.com>
2021-04-05 17:13:04 +0530

    Create database function to check if user has preferences

---
+++ lib/service/database_service.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:la_loge/service/collection_path.dart';
+import 'package:la_loge/service/firebase_auth.dart';
+
+class DatabaseService {
+  final _db = FirebaseFirestore.instance;
+
+  Future<bool> hasPreferences() async {
+    final doc = await _db
+        .collection(CollectionPath.user)
+        .doc(FirebaseAuthentication.uid)
+        .get();
+    return doc.data().containsKey('size_preferences');
+  }
+}
