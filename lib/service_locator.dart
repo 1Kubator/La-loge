3fcb1b5204239551fe16f8cc6812db88fb4a1214
Viditkhanna <vidit.k@surya-soft.com>
2021-04-05 17:09:46 +0530

    Import service locator

---
+++ lib/service_locator.dart
@@ -0,0 +1,9 @@
+
+import 'package:get_it/get_it.dart';
+import 'package:la_loge/service/database_service.dart';
+
+final GetIt locator = GetIt.instance;
+
+void setupLocator() {
+  locator.registerLazySingleton(() => DatabaseService());
+}
