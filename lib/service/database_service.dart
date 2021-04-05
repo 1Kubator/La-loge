1beffaa78d76c03a81018abddbe6af2c1835abeb
Viditkhanna <vidit.k@surya-soft.com>
2021-04-05 18:12:27 +0530

    Remove object uid
    Could create problems when user goes through login as the object still would be pointing to null, as it is static

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:la_loge/service/collection_path.dart';
-import 'package:la_loge/service/firebase_auth.dart';
 
 class DatabaseService {
   final _db = FirebaseFirestore.instance;
@@ -8,7 +8,7 @@ class DatabaseService {
   Future<bool> hasPreferences() async {
     final doc = await _db
         .collection(CollectionPath.user)
-        .doc(FirebaseAuthentication.uid)
+        .doc(FirebaseAuth.instance.currentUser.uid)
         .get();
     return doc.data().containsKey('size_preferences');
   }
--- lib/service/firebase_auth.dart
+++ lib/service/firebase_auth.dart
@@ -3,7 +3,6 @@ import 'package:la_loge/error_handling/network_exception.dart';
 
 class FirebaseAuthentication {
   static final _firebaseAuth = FirebaseAuth.instance;
-  static final uid = FirebaseAuth.instance.currentUser.uid;
 
   static Future<UserCredential> login(String email, String password) async {
     return _firebaseAuth
