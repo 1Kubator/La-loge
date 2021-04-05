951d3acdf6c4e3ce25bbe36e58b86e42e60cd886
Viditkhanna <vidit.k@surya-soft.com>
2021-04-05 17:15:45 +0530

    Use progress dialog

--- lib/widgets/submit_button.dart
+++ lib/widgets/submit_button.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:la_loge/widgets/dialog_box.dart';
+import 'package:la_loge/widgets/progress_dialog.dart';
 import 'loading_widget.dart';
 
 class SubmitButton extends StatelessWidget {
   final String title;
   final Function onTap;
+  final ProgressDialog progressDialog = ProgressDialog();
 
-  const SubmitButton(this.title, {Key key, @required this.onTap})
-      : super(key: key);
+  SubmitButton(this.title, {Key key, @required this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +17,14 @@ class SubmitButton extends StatelessWidget {
         widthFactor: 0.9,
         child: ElevatedButton(
           onPressed: () async {
-            LoadingWidget.showLoadingDialog(context);
+            progressDialog.show(context);
             try {
               await onTap();
             } catch (e) {
               await DialogBox.showErrorDialog(context, e);
+            } finally {
+              progressDialog.hide();
             }
-            Navigator.pop(context);
           },
           child: Text(
             title,
