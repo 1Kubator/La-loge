8404911fb73ef7da3c9c7b8df88f633511ca57bb
Viditkhanna <vidit.k@surya-soft.com>
2021-04-07 10:43:25 +0530

    Add Navigation to SizePreferenceScreen

--- lib/ui/onboarding/onboarding_screen.dart
+++ lib/ui/onboarding/onboarding_screen.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:la_loge/ui/preferences/size_preference_screen.dart';
 import 'package:la_loge/widgets/app_title.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:la_loge/widgets/submit_button.dart';
@@ -65,7 +66,9 @@ class OnBoardingScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 40),
             child: SubmitButton(
               AppLocalizations.of(context).continueWithForm,
-              onTap: () {},
+              onTap: () {
+                Navigator.pushNamed(context, SizePreferenceScreen.id);
+              },
             ),
           )
         ],
