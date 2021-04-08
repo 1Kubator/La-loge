2b7da25a444642c3b24d14bab9a338ee658e9af9
Viditkhanna <vidit.k@surya-soft.com>
2021-04-08 16:31:04 +0530

    Add function for getting material-preference questions & uploadpreferences
    Upload preferences will upload all types of preferences in users collection

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -1,7 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:la_loge/models/all_preferences.dart';
+import 'package:la_loge/models/material_preference.dart';
+import 'package:la_loge/models/material_preference_response.dart';
+import 'package:la_loge/models/option.dart';
 import 'package:la_loge/models/size_preference.dart';
+import 'package:la_loge/models/size_preference_response.dart';
 import 'package:la_loge/models/style_preference.dart';
+import 'package:la_loge/models/style_preference_response.dart';
 import 'package:la_loge/service/collection_path.dart';
 
 class DatabaseService {
@@ -45,5 +51,70 @@ class DatabaseService {
 
     preferences.sort((a, b) => a.index.compareTo(b.index));
     return preferences;
+  }
+
+  Future<List<MaterialPreference>> getMaterialPreferenceQuestions() async {
+    final preferencesDocs =
+        await _db.collection(CollectionPath.materialPreferences).get();
+
+    var preferences = MaterialPreference.fromDocuments(preferencesDocs.docs);
+
+    for (var p in preferences) {
+      var optionDocs = await _db
+          .collection(CollectionPath.materialPreferences)
+          .doc(p.id)
+          .collection(CollectionPath.options)
+          .get();
+      var options = Option.fromDocuments(optionDocs.docs);
+      options.sort((a, b) => a.index.compareTo(b.index));
+
+      preferences[preferences.indexOf(p)] = p.copyWith(options: options);
+    }
+
+    preferences.sort((a, b) => a.index.compareTo(b.index));
+    return preferences;
+  }
+
+  Future<void> uploadPreferences(AllPreferences allPreferences) async {
+    await uploadSizePreferences(allPreferences.sizePreferenceResponse);
+    await uploadStylePreferences(allPreferences.stylePreferenceResponse);
+    await uploadMaterialPreferences(allPreferences.materialPreferenceResponse);
+    return;
+  }
+
+  Future<void> uploadSizePreferences(
+      List<SizePreferenceResponse> preferences) async {
+    for (var preference in preferences) {
+      await _db
+          .collection(CollectionPath.user)
+          .doc(FirebaseAuth.instance.currentUser.uid)
+          .collection(CollectionPath.sizePreferences)
+          .doc()
+          .set(preference.toMap());
+    }
+  }
+
+  Future<void> uploadStylePreferences(
+      List<StylePreferenceResponse> preferences) async {
+    for (var preference in preferences) {
+      await _db
+          .collection(CollectionPath.user)
+          .doc(FirebaseAuth.instance.currentUser.uid)
+          .collection(CollectionPath.stylePreferences)
+          .doc()
+          .set(preference.toMap());
+    }
+  }
+
+  Future<void> uploadMaterialPreferences(
+      List<MaterialPreferenceResponse> preferences) async {
+    for (var preference in preferences) {
+      await _db
+          .collection(CollectionPath.user)
+          .doc(FirebaseAuth.instance.currentUser.uid)
+          .collection(CollectionPath.materialPreferences)
+          .doc()
+          .set(preference.toMap());
+    }
   }
 }
