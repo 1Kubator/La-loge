bf9afd3562059acb4a2dde24f010695e09e3ceb1
Viditkhanna <vidit.k@surya-soft.com>
2021-04-08 16:30:01 +0530

    Add collection path for material-preferences

--- lib/service/collection_path.dart
+++ lib/service/collection_path.dart
@@ -2,5 +2,6 @@ class CollectionPath {
   static String user = "users";
   static String sizePreferences = "size_preferences";
   static String stylePreferences = "style_preferences";
+  static String materialPreferences = "material_preferences";
   static String options = "options";
 }
