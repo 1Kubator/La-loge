492aac3467b4e4428b8f982fb39426d6da7cb31b
Viditkhanna <vidit.k@surya-soft.com>
2021-04-08 16:29:19 +0530

    Add icon for material-preference step image

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -1,4 +1,6 @@
 class Images {
   static const sizePreferenceStep = 'assets/icons/size_preference_step.png';
   static const stylePreferenceStep = 'assets/icons/style_preference_step.png';
+  static const materialPreferenceStep =
+      'assets/icons/material_preference_step.png';
 }
