72b69cce3e051da0a2f43375b3affcedaaf8c392
Viditkhanna <vidit.k@surya-soft.com>
2021-04-09 18:07:05 +0530

    Added dependencies for crashlytics and analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:la_loge/service_locator.dart';
 import 'app.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   setupLocator();
-  runApp(MyApp());
+  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(!kDebugMode);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  runZonedGuarded(() {
+    runApp(MyApp());
+  }, FirebaseCrashlytics.instance.recordError);
 }
