2982cb955464343a3d5c707b395f9dc29f4cb7f4
Viditkhanna <vidit.k@surya-soft.com>
2021-04-09 18:07:56 +0530

    Add analytics service class

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,8 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:la_loge/service/database_service.dart';
 
+import 'service/analytics_service.dart';
+
 final GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerLazySingleton(() => DatabaseService());
+  locator.registerLazySingleton(() => AnalyticsService());
 }
