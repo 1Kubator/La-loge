ade06d0dc4835d8b38b75ce3f903a9baaed1b7d5
Viditkhanna <vidit.k@surya-soft.com>
2021-04-15 14:30:26 +0530

    Remove unused imports

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,9 +1,7 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:la_loge/prefs/shared_prefs.dart';
 import 'package:la_loge/resources/images.dart';
 import 'package:la_loge/widgets/app_title.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
