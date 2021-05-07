1eeeb949c0c31a5abe5e1ed0586e63eec65a0657
Viditkhanna <vidit.k@surya-soft.com>
2021-05-07 16:44:24 +0530

    Create appointment-list-screen

---
+++ lib/ui/appointment/appointment_list_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:intl/intl.dart';
+import 'package:la_loge/models/store_appointment_argument.dart';
+import 'package:la_loge/service/database_service.dart';
+import 'package:la_loge/service_locator.dart';
+import 'package:la_loge/ui/appointment/widgets/appointment_card.dart';
+import 'package:la_loge/widgets/app_title.dart';
+import 'package:la_loge/widgets/error_box.dart';
+import 'package:la_loge/widgets/loading_animation.dart';
+
+class AppointmentListScreen extends StatefulWidget {
+  static const id = 'appointment_list_screen';
+
+  @override
+  _AppointmentListScreenState createState() => _AppointmentListScreenState();
+}
+
+class _AppointmentListScreenState extends State<AppointmentListScreen> {
+  final db = locator<DatabaseService>();
+  final dateFormat = DateFormat.yMMMMEEEEd('fr');
+  final timeFormat = DateFormat.Hm('fr');
+  Stream<List<StoreAppointmentArgument>> stream;
+
+  @override
+  void initState() {
+    super.initState();
+    stream = db.getAppointments();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<StoreAppointmentArgument>>(
+        stream: stream,
+        builder: (context, snap) {
+          print(snap.error);
+          if (snap.hasError) return Center(child: ErrorBox(error: snap.error));
+          if (!snap.hasData) return LoadingAnimation();
+          return Scaffold(
+            appBar: AppBar(),
+            body: ListView(
+              children: [
+                Center(child: AppTitle()),
+                SizedBox(height: 40),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 55 / 100,
+                  child: Swiper(
+                    itemCount: snap.data.length,
+                    viewportFraction: 0.85,
+                    scale: 0.95,
+                    loop: false,
+                    itemBuilder: (context, index) {
+                      return AppointmentCard(
+                        dateFormat: dateFormat,
+                        storeAppointmentArg: snap.data[index],
+                        timeFormat: timeFormat,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+}
---
+++ lib/ui/appointment/widgets/appointment_card.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:la_loge/models/store_appointment_argument.dart';
+import 'package:la_loge/utils/app_localizations.dart';
+
+class AppointmentCard extends StatelessWidget {
+  final StoreAppointmentArgument storeAppointmentArg;
+  final DateFormat dateFormat;
+  final DateFormat timeFormat;
+
+  const AppointmentCard(
+      {Key key, this.storeAppointmentArg, this.dateFormat, this.timeFormat})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40.0,
+          vertical: 24,
+        ),
+        child: Column(
+          children: [
+            Text(
+              MyAppLocalizations.of(context).previousPrivateShopping,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+                color: Theme.of(context).primaryColorDark,
+                fontFamily: GoogleFonts.playfairDisplay().fontFamily,
+              ),
+            ),
+            SizedBox(height: 24),
+            Text(
+              storeAppointmentArg.store.name,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+                color: Theme.of(context).primaryColorDark,
+                fontFamily: GoogleFonts.inter().fontFamily,
+              ),
+            ),
+            Text(
+              storeAppointmentArg.store.location,
+              style: TextStyle(
+                fontSize: 12,
+                color: Theme.of(context).primaryColorDark,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              dateFormat.format(
+                  storeAppointmentArg.storeAppointment.appointmentDateTime),
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+                fontFamily: GoogleFonts.inter().fontFamily,
+                color: Theme.of(context).primaryColorDark,
+              ),
+            ),
+            Text(
+              timeFormat
+                  .format(
+                      storeAppointmentArg.storeAppointment.appointmentDateTime)
+                  .replaceFirst(':', 'h'),
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+                fontFamily: GoogleFonts.inter().fontFamily,
+                color: Theme.of(context).primaryColorDark,
+              ),
+            ),
+            SizedBox(height: 24),
+            Text(
+              storeAppointmentArg.appointmentReason,
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).primaryColorDark,
+              ),
+            ),
+            SizedBox(height: 24),
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: OutlinedButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(
+                    Theme.of(context).primaryColorDark,
+                  ),
+                  side: MaterialStateProperty.all(
+                    BorderSide(
+                      color: Theme.of(context).primaryColorDark,
+                      width: 0.7,
+                    ),
+                  ),
+                ),
+                child: Text(
+                  MyAppLocalizations.of(context).modify,
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
