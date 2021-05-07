de224eda13b0e835c65a03542303898a11ec07e2
Viditkhanna <vidit.k@surya-soft.com>
2021-05-07 20:11:05 +0530

    Add route for edit-appointment-screen

--- lib/ui/appointment/appointment_screen_navigator.dart
+++ lib/ui/appointment/appointment_screen_navigator.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:la_loge/models/store_appointment.dart';
 import 'package:la_loge/ui/appointment/appointment_list_screen.dart';
+import 'package:la_loge/ui/appointment/edit_appointment_screen.dart';
 
 class AppointmentScreenNavigator extends StatelessWidget {
   static const id = 'appointment_screen_navigator';
@@ -11,13 +13,23 @@ class AppointmentScreenNavigator extends StatelessWidget {
         key: navigationKey,
         initialRoute: AppointmentListScreen.id,
         onGenerateRoute: (RouteSettings settings) {
+          final args = settings.arguments;
           switch (settings.name) {
             case AppointmentListScreen.id:
               return MaterialPageRoute(
-                fullscreenDialog: true,
                 settings: RouteSettings(name: AppointmentListScreen.id),
                 builder: (context) => AppointmentListScreen(),
               );
+            case EditAppointmentScreen.id:
+              if (args is StoreAppointment) {
+                return MaterialPageRoute(
+                  settings: RouteSettings(name: EditAppointmentScreen.id),
+                  builder: (context) => EditAppointmentScreen(
+                    storeAppointment: args,
+                  ),
+                );
+              }
+              throw 'Invalid route or arguments';
             default:
               return MaterialPageRoute(
                 settings: RouteSettings(name: AppointmentListScreen.id),
