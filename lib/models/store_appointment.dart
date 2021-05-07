d3b7003075260d7459b3b3a97a849b9583c35195
Viditkhanna <vidit.k@surya-soft.com>
2021-05-07 16:38:19 +0530

    Add store-id
    Store-id will be saved in DB in an appointment document

--- lib/models/store_appointment.dart
+++ lib/models/store_appointment.dart
@@ -1,15 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class StoreAppointment {
   StoreAppointment({
+    this.id,
     this.bookingQuestions,
     this.status,
     this.appointmentDateTime,
     this.userId,
+    this.storeId,
   });
 
+  final String id;
   final Map<String, dynamic> bookingQuestions;
   final String status;
   final DateTime appointmentDateTime;
   final String userId;
+  final String storeId;
 
   StoreAppointment copyWith({
     Map<String, dynamic> bookingQuestions,
@@ -18,29 +24,37 @@ class StoreAppointment {
     String userId,
   }) =>
       StoreAppointment(
+        id: id ?? this.id,
         bookingQuestions: bookingQuestions ?? this.bookingQuestions,
         status: status ?? this.status,
         appointmentDateTime: appointmentTimeStamp ?? this.appointmentDateTime,
         userId: userId ?? this.userId,
+        storeId: storeId ?? this.storeId,
       );
 
-  factory StoreAppointment.fromMap(Map<String, dynamic> json) =>
+  factory StoreAppointment.fromMap(String id, Map<String, dynamic> json) =>
       StoreAppointment(
+        id: id,
         bookingQuestions: json["booking_questions"] == null
             ? null
             : json["booking_questions"],
         status: json["status"] == null ? null : json["status"],
         appointmentDateTime: json["appointment_date_time"] == null
             ? null
-            : json["appointment_date_time"],
+            : json["appointment_date_time"] is Timestamp
+                ? json["appointment_date_time"].toDate()
+                : json["appointment_date_time"],
         userId: json["user_id"] == null ? null : json["user_id"],
+        storeId: json["store_id"] == null ? null : json["store_id"],
       );
 
   Map<String, dynamic> toMap() => {
+        "id": id == null ? null : id,
         "booking_questions": bookingQuestions == null ? null : bookingQuestions,
         "status": status == null ? null : status,
         "appointment_date_time":
             appointmentDateTime == null ? null : appointmentDateTime,
         "user_id": userId == null ? null : userId,
+        "store_id": storeId == null ? null : storeId,
       };
 }
