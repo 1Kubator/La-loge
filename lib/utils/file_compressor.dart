4af5a98da8053db54cdbbe37e06b2c1210866d96
Viditkhanna <vidit.k@surya-soft.com>
2021-05-25 13:44:21 +0530

    Add file compressor

---
+++ lib/utils/file_compressor.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:path_provider/path_provider.dart';
+
+class FileCompressor {
+  static Future<File> compressImg(File file) async {
+    final tempDir = await getApplicationDocumentsDirectory();
+    var imgFormat=file.path.split('.').last;
+    print(imgFormat);
+    var result = await FlutterImageCompress.compressAndGetFile(
+      file.absolute.path,
+      tempDir.path + '.$imgFormat',
+      quality: 70,
+    );
+    print(file.lengthSync());
+    print(result.lengthSync());
+    return result;
+  }
+}
