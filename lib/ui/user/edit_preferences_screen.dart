d189bffd76125165bf68b03fb8711842ef884b38
Viditkhanna <vidit.k@surya-soft.com>
2021-05-26 14:21:05 +0530

    Use preference widget

--- lib/ui/user/edit_preferences_screen.dart
+++ lib/ui/user/edit_preferences_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:la_loge/ui/user/widgets/edit_material_preference.dart';
 import 'package:la_loge/ui/user/widgets/edit_size_preference.dart';
 import 'package:la_loge/utils/app_localizations.dart';
 import 'package:la_loge/widgets/app_title.dart';
+
+import 'widgets/edit_style_preference.dart';
 
 class EditPreferencesScreen extends StatefulWidget {
   static const id = 'edit_preferences_screen';
@@ -17,6 +20,7 @@ class _EditPreferencesScreenState extends State<EditPreferencesScreen> {
     return Scaffold(
       appBar: AppBar(),
       body: ListView(
+        padding: EdgeInsets.only(bottom: 20),
         children: [
           Center(child: AppTitle()),
           SizedBox(height: 40),
@@ -41,8 +45,42 @@ class _EditPreferencesScreenState extends State<EditPreferencesScreen> {
               ),
             ),
           ),
-          SizedBox(height: 24),
+          SizedBox(height: 16),
           EditSizePreferences(),
+          SizedBox(height: 32),
+          Center(
+            child: Text(
+              MyAppLocalizations.of(context).style,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 18,
+                fontFamily: GoogleFonts.inter().fontFamily,
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          EditStylePreferences(),
+          SizedBox(height: 32),
+          Center(
+            child: Text(
+              MyAppLocalizations.of(context).materialPreference,
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 18,
+                fontFamily: GoogleFonts.inter().fontFamily,
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          EditMaterialPreferences(),
+          SizedBox(height: 28),
+          FractionallySizedBox(
+            widthFactor: 0.8,
+            child: OutlinedButton(
+              onPressed: () {},
+              child: Text(MyAppLocalizations.of(context).seeSelectedArticles),
+            ),
+          )
         ],
       ),
     );
