dc44227b99ef7e2caa3ab2537b0d974f257c3e8a
Viditk <79438214+Viditk@users.noreply.github.com>
2021-05-27 17:21:31 +0530

    Fix appointment-card-design issue (#49)

--- lib/ui/appointment/widgets/appointment_card.dart
+++ lib/ui/appointment/widgets/appointment_card.dart
@@ -18,103 +18,108 @@ class AppointmentCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 8),
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 40.0,
-            vertical: 24,
-          ),
-          child: Column(
-            children: [
-              Text(
-                MyAppLocalizations.of(context).previousPrivateShopping,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                  color: Theme.of(context).primaryColorDark,
-                  fontFamily: GoogleFonts.playfairDisplay().fontFamily,
+      child: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 40.0,
+              vertical: 24,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  MyAppLocalizations.of(context).previousPrivateShopping,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                    color: Theme.of(context).primaryColorDark,
+                    fontFamily: GoogleFonts.playfairDisplay().fontFamily,
+                  ),
                 ),
-              ),
-              SizedBox(height: 24),
-              Text(
-                storeAppointmentArg.store.name,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700,
-                  color: Theme.of(context).primaryColorDark,
-                  fontFamily: GoogleFonts.inter().fontFamily,
+                SizedBox(height: 24),
+                Text(
+                  storeAppointmentArg.store.name,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: Theme.of(context).primaryColorDark,
+                    fontFamily: GoogleFonts.inter().fontFamily,
+                  ),
                 ),
-              ),
-              Text(
-                storeAppointmentArg.store.location,
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Theme.of(context).primaryColorDark,
+                Text(
+                  storeAppointmentArg.store.location,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Theme.of(context).primaryColorDark,
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                dateFormat.format(
-                    storeAppointmentArg.storeAppointment.appointmentDateTime),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700,
-                  fontFamily: GoogleFonts.inter().fontFamily,
-                  color: Theme.of(context).primaryColorDark,
+                SizedBox(height: 20),
+                Text(
+                  dateFormat.format(
+                      storeAppointmentArg.storeAppointment.appointmentDateTime),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: GoogleFonts.inter().fontFamily,
+                    color: Theme.of(context).primaryColorDark,
+                  ),
                 ),
-              ),
-              Text(
-                timeFormat
-                    .format(storeAppointmentArg
-                        .storeAppointment.appointmentDateTime)
-                    .replaceFirst(':', 'h'),
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700,
-                  fontFamily: GoogleFonts.inter().fontFamily,
-                  color: Theme.of(context).primaryColorDark,
+                Text(
+                  timeFormat
+                      .format(storeAppointmentArg
+                          .storeAppointment.appointmentDateTime)
+                      .replaceFirst(':', 'h'),
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: GoogleFonts.inter().fontFamily,
+                    color: Theme.of(context).primaryColorDark,
+                  ),
                 ),
-              ),
-              SizedBox(height: 24),
-              Text(
-                storeAppointmentArg.appointmentReason,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Theme.of(context).primaryColorDark,
+                SizedBox(height: 24),
+                Text(
+                  storeAppointmentArg.appointmentReason,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).primaryColorDark,
+                  ),
                 ),
-              ),
-              SizedBox(height: 24),
-              FractionallySizedBox(
-                widthFactor: 1,
-                child: OutlinedButton(
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all(
-                      Theme.of(context).primaryColorDark,
-                    ),
-                    side: MaterialStateProperty.all(
-                      BorderSide(
-                        color: Theme.of(context).primaryColorDark,
-                        width: 0.7,
+                if (storeAppointmentArg.storeAppointment.appointmentDateTime
+                    .isAfter(DateTime.now())) ...[
+                  SizedBox(height: 24),
+                  FractionallySizedBox(
+                    widthFactor: 1,
+                    child: OutlinedButton(
+                      style: ButtonStyle(
+                        foregroundColor: MaterialStateProperty.all(
+                          Theme.of(context).primaryColorDark,
+                        ),
+                        side: MaterialStateProperty.all(
+                          BorderSide(
+                            color: Theme.of(context).primaryColorDark,
+                            width: 0.7,
+                          ),
+                        ),
                       ),
+                      child: Text(
+                        MyAppLocalizations.of(context).modify,
+                        textAlign: TextAlign.center,
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(
+                          context,
+                          EditAppointmentScreen.id,
+                          arguments: storeAppointmentArg.storeAppointment,
+                        );
+                      },
                     ),
                   ),
-                  child: Text(
-                    MyAppLocalizations.of(context).modify,
-                    textAlign: TextAlign.center,
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(
-                      context,
-                      EditAppointmentScreen.id,
-                      arguments: storeAppointmentArg.storeAppointment,
-                    );
-                  },
-                ),
-              ),
-            ],
+                ],
+              ],
+            ),
           ),
         ),
       ),
