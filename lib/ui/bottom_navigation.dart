225be3969669d38b83e25953bef44679cdbce49c
Viditk <79438214+Viditk@users.noreply.github.com>
2021-05-27 15:12:56 +0530

    Fix back button issue (#44)

--- lib/ui/bottom_navigation.dart
+++ lib/ui/bottom_navigation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:la_loge/providers/tabs_notifier.dart';
 import 'package:la_loge/resources/images.dart';
 import 'package:la_loge/ui/appointment/appointment_screen_navigator.dart';
@@ -23,42 +24,58 @@ class _BottomNavigationState extends State<BottomNavigation> {
     MenuNavigator(),
   ];
 
+  // ignore: missing_return
+  Future<bool> _systemBackButtonPressed() {
+    final tabNotifier = Provider.of<TabsNotifier>(context, listen: false);
+    dynamic key = screens[tabNotifier.currentTabIndex];
+    if (key.navigationKey.currentState.canPop()) {
+      key.navigationKey.currentState.pop(key.navigationKey.currentContext);
+    } else {
+      SystemChannels.platform.invokeMethod<void>('SystemNavigator.pop');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final tabNotifier = Provider.of<TabsNotifier>(context);
-    return Scaffold(
-      body: IndexedStack(
-        index: tabNotifier.currentTabIndex,
-        children: screens,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) {
-          tabNotifier.setTabIndex = index;
-        },
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Theme.of(context).primaryColorDark,
-        currentIndex: tabNotifier.currentTabIndex,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: [
-          BottomNavigationBarItem(
-              icon: getUnselectedThemedIcon(Images.navBarHome, context),
-              label: '',
-              activeIcon: getSelectedThemedIcon(Images.navBarHome, context)),
-          BottomNavigationBarItem(
-              icon: getUnselectedThemedIcon(Images.navBarStore, context),
-              label: '',
-              activeIcon: getSelectedThemedIcon(Images.navBarStore, context)),
-          BottomNavigationBarItem(
-              icon: getUnselectedThemedIcon(Images.navBarAppointment, context),
-              label: '',
-              activeIcon:
-                  getSelectedThemedIcon(Images.navBarAppointment, context)),
-          BottomNavigationBarItem(
-              icon: getUnselectedThemedIcon(Images.navBarProfile, context),
-              label: '',
-              activeIcon: getSelectedThemedIcon(Images.navBarProfile, context)),
-        ],
+    return WillPopScope(
+      onWillPop: _systemBackButtonPressed,
+      child: Scaffold(
+        body: IndexedStack(
+          index: tabNotifier.currentTabIndex,
+          children: screens,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            tabNotifier.setTabIndex = index;
+          },
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Theme.of(context).primaryColorDark,
+          currentIndex: tabNotifier.currentTabIndex,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+                icon: getUnselectedThemedIcon(Images.navBarHome, context),
+                label: '',
+                activeIcon: getSelectedThemedIcon(Images.navBarHome, context)),
+            BottomNavigationBarItem(
+                icon: getUnselectedThemedIcon(Images.navBarStore, context),
+                label: '',
+                activeIcon: getSelectedThemedIcon(Images.navBarStore, context)),
+            BottomNavigationBarItem(
+                icon:
+                    getUnselectedThemedIcon(Images.navBarAppointment, context),
+                label: '',
+                activeIcon:
+                    getSelectedThemedIcon(Images.navBarAppointment, context)),
+            BottomNavigationBarItem(
+                icon: getUnselectedThemedIcon(Images.navBarProfile, context),
+                label: '',
+                activeIcon:
+                    getSelectedThemedIcon(Images.navBarProfile, context)),
+          ],
+        ),
       ),
     );
   }
