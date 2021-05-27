89c60d13e786862cf5c895763cff2236e712b940
Viditk <79438214+Viditk@users.noreply.github.com>
2021-05-27 16:45:52 +0530

    Add no/empty appointment text (#46)

--- lib/ui/appointment/appointment_list_screen.dart
+++ lib/ui/appointment/appointment_list_screen.dart
@@ -5,6 +5,7 @@ import 'package:la_loge/models/store_appointment_argument.dart';
 import 'package:la_loge/service/database_service.dart';
 import 'package:la_loge/service_locator.dart';
 import 'package:la_loge/ui/appointment/widgets/appointment_card.dart';
+import 'package:la_loge/utils/app_localizations.dart';
 import 'package:la_loge/widgets/app_title.dart';
 import 'package:la_loge/widgets/error_box.dart';
 import 'package:la_loge/widgets/loading_animation.dart';
@@ -30,6 +31,7 @@ class _AppointmentListScreenState extends State<AppointmentListScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return StreamBuilder<List<StoreAppointmentArgument>>(
         stream: stream,
         builder: (context, snap) {
@@ -41,22 +43,33 @@ class _AppointmentListScreenState extends State<AppointmentListScreen> {
               children: [
                 Center(child: AppTitle()),
                 SizedBox(height: 40),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 55 / 100,
-                  child: Swiper(
-                    itemCount: snap.data.length,
-                    viewportFraction: 0.85,
-                    scale: 0.95,
-                    loop: false,
-                    itemBuilder: (context, index) {
-                      return AppointmentCard(
-                        dateFormat: dateFormat,
-                        storeAppointmentArg: snap.data[index],
-                        timeFormat: timeFormat,
-                      );
-                    },
+                if (snap.data.isEmpty)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Center(
+                      child: Text(
+                        MyAppLocalizations.of(context).noAppointments,
+                        style: textTheme.caption,
+                      ),
+                    ),
+                  )
+                else
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 55 / 100,
+                    child: Swiper(
+                      itemCount: snap.data.length,
+                      viewportFraction: 0.85,
+                      scale: 0.95,
+                      loop: false,
+                      itemBuilder: (context, index) {
+                        return AppointmentCard(
+                          dateFormat: dateFormat,
+                          storeAppointmentArg: snap.data[index],
+                          timeFormat: timeFormat,
+                        );
+                      },
+                    ),
                   ),
-                ),
               ],
             ),
           );
