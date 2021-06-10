7768d4a13b42ffcf91cb28a8dcfc6474b5d09101
Vidit Khanna <>
2021-06-10 14:53:39 +0530

    Remove unused imports

--- lib/widgets/app_title.dart
+++ lib/widgets/app_title.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:la_loge/resources/images.dart';
 
 class AppTitle extends StatelessWidget {
