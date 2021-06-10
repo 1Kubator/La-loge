7d3ff1afbbf8f2fdd12c48370e02614ca48ee095
Vidit Khanna <>
2021-06-10 14:44:59 +0530

    Return app-icon widget

--- lib/widgets/app_title.dart
+++ lib/widgets/app_title.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:la_loge/resources/images.dart';
 
 class AppTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      AppLocalizations.of(context).appTitle,
-      style: TextStyle(
-        fontSize: 30,
-        fontWeight: FontWeight.w700,
-        fontFamily: GoogleFonts.inter().fontFamily,
-      ),
+    final size = MediaQuery.of(context).size;
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: size.width * 0.12),
+      child: Image.asset(Images.appIcon),
     );
   }
 }
